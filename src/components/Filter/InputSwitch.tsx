7f862b0f0341739d63976b7407dbe8d26975c5ce
Lisa Årman <lisa.arman@toxic.se>
2023-11-03 10:13:16 +0100

    Input switch field name

--- src/components/Filter/InputSwitch.tsx
+++ src/components/Filter/InputSwitch.tsx
@@ -16,7 +16,7 @@ const InputSwitch = ({ option }: Props) => {
         <InputField
           placeholder={`${t('Filter.Enter')} ${option.label}`}
           variant="filled"
-          name={option.label.toLowerCase().replace(/ /g, '')}
+          name={option.value.name}
         />
       );
     case 'select':
