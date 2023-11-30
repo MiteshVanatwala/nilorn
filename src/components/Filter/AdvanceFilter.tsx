57b6aca44b8f861345fe8aa1d6388cc3ae26d987
Linnea Karlsson <linnea.d.karlsson@gmail.com>
2023-11-30 16:31:51 +0100

    5753 Ypdate how to get labvels for active filters

--- src/components/Filter/AdvanceFilter.tsx
+++ src/components/Filter/AdvanceFilter.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ActionMeta, MultiValue } from 'chakra-react-select';
 import {
   Accordion,
@@ -26,16 +26,24 @@ import AdvanceFilterSelect from './AdvanceFilterSelect';
 import FilterSwitch from './FilterSwitch';
 
 type Props = {
-  filters: SelectOption<AdvanceFilterType>[];
+  filters: AdvanceFilterType[];
 };
 
 const AdvanceFilter = ({ filters }: Props) => {
   const { t } = useTranslation();
   const { unregister, getValues } = useFormContext();
-
   const [selected, setSelected] = useState<
     MultiValue<SelectOption<AdvanceFilterType>>
   >([]);
+
+  const options = useMemo(
+    () =>
+      filters.map(f => ({
+        label: t(`PD.FilterLabel.${f.name}`),
+        value: f,
+      })),
+    [filters, t]
+  );
 
   const handleSelect = (
     selectedOption: MultiValue<SelectOption<AdvanceFilterType>> | undefined,
@@ -62,7 +70,7 @@ const AdvanceFilter = ({ filters }: Props) => {
     const activeAdvancedFilterArr: SelectOption[] = [];
 
     Object.entries(getValues()).forEach(([key, value]) => {
-      filters?.forEach(filterItem => {
+      options?.forEach(filterItem => {
         if (
           filterItem &&
           'value' in filterItem &&
@@ -78,7 +86,8 @@ const AdvanceFilter = ({ filters }: Props) => {
     if (activeAdvancedFilterArr.length) {
       setSelected(activeAdvancedFilterArr);
     }
-  }, [getValues, filters]);
+  }, [getValues, filters, options]);
+
   return (
     <Accordion allowToggle index={index} onChange={setIndex}>
       <AccordionItem border={'none'} overflow={'visible'}>
@@ -114,7 +123,7 @@ const AdvanceFilter = ({ filters }: Props) => {
             alignItems={'center'}>
             <GridItem colSpan={2} zIndex={9}>
               <AdvanceFilterSelect
-                options={filters}
+                options={options}
                 value={selected}
                 onChange={(option, event) => {
                   handleSelect(option, event);
--- src/components/Filter/ProductDevelopmentFilter.tsx
+++ src/components/Filter/ProductDevelopmentFilter.tsx
@@ -4,7 +4,7 @@ import InputSearch from '../Form/InputSearch';
 import { useTranslation } from 'react-i18next';
 import SearchProfile from '../SearchProfile/SearchProfile';
 import ActiveFilters from './ActiveFilters';
-import { useOverviewAdvanceFilters } from '../../app/hooks/useOverviewAdvanceFilters';
+import { getOverviewAdvanceFilters } from '../../app/utils/getOverviewAdvanceFilters';
 import AdvanceFilter from './AdvanceFilter';
 import { GRID, SPACE } from '../../theme/Constants';
 import FilterSelect from './FilterSelect';
@@ -19,7 +19,7 @@ import { useEffect } from 'react';
 const ProductDevelopmentFilter = () => {
   const { t } = useTranslation();
   const form = useForm();
-  const advanceFilters = useOverviewAdvanceFilters();
+  const advanceFilters = getOverviewAdvanceFilters();
 
   const clientOptions = useFilterOptions('clients');
   const statusOptions = useStatusOptions();
