458e2154c4ebd5bf05bf27cd8438fd971b9b45d4
Linnea Karlsson <linnea.d.karlsson@gmail.com>
2023-12-06 11:56:25 +0100

    5731 Prodction table as a table

---
+++ src/pages/Productions/ProductDevelopmentCell.tsx
@@ -0,0 +1,51 @@
+import { Box, Text, Image, IconButton, HStack, VStack } from '@chakra-ui/react';
+import StatusBadge from '../../components/Status/StatusBadge';
+import { ProductDevelopmentBriefDto, Status } from '../../app/generate';
+import { images } from '../../assets';
+import { useTranslation } from 'react-i18next';
+import { SPACE } from '../../theme/Constants';
+
+const ProductDevelopmentCell = ({
+  number,
+  name,
+  artworkUrl,
+  imageUrl,
+  status,
+  project,
+}: ProductDevelopmentBriefDto) => {
+  const { t } = useTranslation();
+
+  return (
+    <Box>
+      <VStack spacing={SPACE.MD} alignItems={'baseline'}>
+        <HStack justifyContent={'space-between'} width={'100%'}>
+          <Text>#{number}</Text>
+
+          {artworkUrl && (
+            <IconButton
+              variant={'ghost'}
+              aria-label={t('PD.Artwork')}
+              onClick={() => console.log('Download')}
+              icon={
+                <Image
+                  src={images.pdf}
+                  height="2.5rem"
+                  objectFit={'contain'}
+                  width="auto"
+                />
+              }
+            />
+          )}
+        </HStack>
+        <Text variant={'bodyBigBlack'}>{name}</Text>
+        {imageUrl && (
+          <Image height={'8rem'} objectFit={'cover'} src={imageUrl} />
+        )}
+        {project && <Text>{project}</Text>}
+        <StatusBadge status={status as Status} />
+      </VStack>
+    </Box>
+  );
+};
+
+export default ProductDevelopmentCell;
