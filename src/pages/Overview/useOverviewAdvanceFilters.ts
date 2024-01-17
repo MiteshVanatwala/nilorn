8adf5243c2afe1a2fdeccb2fe47130f4b3aae393
Lisa Årman <lisa.arman@toxic.se>
2024-01-17 11:20:59 +0100

    Added back filters to overview

--- src/pages/Overview/useOverviewAdvanceFilters.ts
+++ src/pages/Overview/useOverviewAdvanceFilters.ts
@@ -16,5 +16,33 @@ export const useOverviewAdvanceFilters = (): SelectOption<FilterInput>[] => {
       label: t(`PD.FilterLabel.foldingTypes`),
       value: { type: 'select', name: 'foldingTypes' },
     },
+    {
+      label: t(`PD.FilterLabel.finishedLengths`),
+      value: { type: 'text', name: 'finishedLengths' },
+    },
+    {
+      label: t(`PD.FilterLabel.finishedWidths`),
+      value: { type: 'text', name: 'finishedWidths' },
+    },
+    {
+      label: t(`PD.FilterLabel.finishedHeights`),
+      value: { type: 'text', name: 'finishedHeights' },
+    },
+    {
+      label: t(`PD.FilterLabel.vendor`),
+      value: { type: 'select', name: 'vendor' },
+    },
+    {
+      label: t(`PD.FilterLabel.opComp`),
+      value: { type: 'select', name: 'opComp' },
+    },
+    {
+      label: t(`PD.FilterLabel.sourcingCompanies`),
+      value: { type: 'select', name: 'sourcingCompanies' },
+    },
+    {
+      label: t(`PD.FilterLabel.salespersonPurchaser`),
+      value: { type: 'select', name: 'salespersonPurchaser' },
+    },
   ];
 };
