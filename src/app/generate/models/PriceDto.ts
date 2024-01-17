bf14d73bcf233b1d7ce503509e645780a1522f27
Linnea Karlsson <linnea.d.karlsson@gmail.com>
2024-01-17 10:05:01 +0100

    Generate API

---
+++ src/app/generate/models/PriceDto.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PriceDto = {
+    salesPrice?: number | null;
+    purchasePrice?: number | null;
+    cost?: number | null;
+    quantity?: number;
+    margin?: number | null;
+};
