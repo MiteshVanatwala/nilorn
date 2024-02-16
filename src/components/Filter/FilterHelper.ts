4f59eb3d034524edd10a23e6be32f92c410f49d2
Lisa Årman <lisa.arman@toxic.se>
2024-02-16 13:30:34 +0100

    Filter helper pathname on productions and calculations

--- src/components/Filter/FilterHelper.ts
+++ src/components/Filter/FilterHelper.ts
@@ -130,11 +130,12 @@ export function getSortState(sortValue: string): SortingState {
 }
 export function getCurrentStoredFilter() {
   let storedFilter = '';
-  if (window.location.pathname === '/productions') {
+  console.log('window.location.pathname', window.location.pathname);
+  if (window.location.pathname.includes('/productions')) {
     storedFilter = 'prevFilterProductions';
   } else if (window.location.pathname === '/') {
     storedFilter = 'prevFilterOverview';
-  } else if (window.location.pathname === '/price-calculations') {
+  } else if (window.location.pathname.includes('/price-calculations')) {
     storedFilter = 'prevFilterCalculation';
   }
   return storedFilter;
