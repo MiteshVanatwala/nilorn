d52d258f4e105772869b0018d07830f44d117393
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-04-12 15:05:38 +0200

    Updated ArrowLink so it can be used as a button

--- src/components/Link/ArrowLink.tsx
+++ src/components/Link/ArrowLink.tsx
@@ -1,33 +1,35 @@
-import { Button } from '@chakra-ui/react';
+import { Button, ButtonProps } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 
 type Props = {
   direction: 'left' | 'right';
   children: JSX.Element;
-  to: string;
+  to?: string;
+  onClick?: () => void;
 };
-const ArrowLink = ({ direction, children, to }: Props) => {
-  return (
-    <>
-      <Button
-        as={NavLink}
-        end
-        to={to}
-        pr={0}
-        variant={direction === 'left' ? 'backButton' : 'forwardButton'}
-        leftIcon={
-          direction === 'left' ? (
-            <i className={`ri-arrow-left-line`} />
-          ) : undefined
-        }
-        rightIcon={
-          direction === 'left' ? undefined : (
-            <i className={`ri-arrow-right-line`} />
-          )
-        }>
+
+const ArrowLink = ({ direction, children, to, onClick }: Props) => {
+  const props: ButtonProps = {
+    pr: 0,
+    variant: direction === 'left' ? 'backButton' : 'forwardButton',
+    leftIcon:
+      direction === 'left' ? <i className={`ri-arrow-left-line`} /> : undefined,
+    rightIcon:
+      direction === 'left' ? undefined : (
+        <i className={`ri-arrow-right-line`} />
+      ),
+  };
+  if (onClick && !to) {
+    return (
+      <Button onClick={onClick} {...props}>
         {children}
       </Button>
-    </>
+    );
+  }
+  return (
+    <Button as={NavLink} end to={to} {...props}>
+      {children}
+    </Button>
   );
 };
 
