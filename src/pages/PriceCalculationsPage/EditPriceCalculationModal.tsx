79fe7ad668eebcabbf26d5050d8b83e0c2ca785e
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-04-12 15:07:44 +0200

    Added unsaved changes to EditPriceCalculationModal

--- src/pages/PriceCalculationsPage/EditPriceCalculationModal.tsx
+++ src/pages/PriceCalculationsPage/EditPriceCalculationModal.tsx
@@ -1,7 +1,13 @@
 import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import { ChangelogType } from '../../app/generate';
-import { Box, Button, HStack, Skeleton } from '@chakra-ui/react';
-import { COLORS, SPACE } from '../../theme/Constants';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Skeleton,
+  useOutsideClick,
+} from '@chakra-ui/react';
+import { GRID, SPACE } from '../../theme/Constants';
 import ProductDevelopmentModalTopSection from '../../components/ProductDevelopment/ProductDevelopmentModalTopSection';
 import PriceCalculationForm from './PriceCalculationForm';
 import PriceCalculationActionBar from './PriceCalculationActionBar';
@@ -10,12 +16,18 @@ import {
   usePriceCalculation,
   usePriceCalculationNavigation,
 } from '../../app/api/calculation';
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { ModalContext } from '../../app/context/ModalContext';
 import { useToggleChangelog } from '../../app/hooks/useChangelog';
 import { ServerFilter } from '../../app/types/types';
 import { useTranslation } from 'react-i18next';
 import SpinnerOverlay from '../../components/Spinner/SpinnerOverlay';
+import LeavePageModal, {
+  ModalRef,
+} from '../../components/Modal/LeavePageModal';
+import { useUnsavedChanges } from '../../app/hooks/useUnsavedChanges';
+import ArrowLink from '../../components/Link/ArrowLink';
+import ContentSection from '../Templates/ContentSection';
 
 type Props = {
   calculationId: string;
@@ -24,6 +36,8 @@ type Props = {
 
 const EditPriceCalculationModal = ({ calculationId, filters }: Props) => {
   const { t } = useTranslation();
+  const outsideRef = useRef(null);
+  const modalRef = useRef<ModalRef>(null);
   const [activeCalculationId, setActiveCalculationId] = useState(calculationId);
   const { data: priceCalculationNavigation } = usePriceCalculationNavigation(
     activeCalculationId,
@@ -34,9 +48,14 @@ const EditPriceCalculationModal = ({ calculationId, filters }: Props) => {
     isLoading,
     isRefetching,
   } = usePriceCalculation(activeCalculationId);
+  const [pendingCalculationId, setPendingCalculationId] = useState<
+    string | undefined
+  >(undefined);
 
   const { mutate: updateCalculation } = usePatchCalculation();
-  const { close } = useContext(ModalContext);
+  const { close, setPreventClose } = useContext(ModalContext);
+  const { discardChanges, setUnsavedChanges, hasUnsavedChanges } =
+    useUnsavedChanges();
   const { showChanges, setShowChanges } = useToggleChangelog(
     ChangelogType.PRICE_CALCULATION,
     undefined,
@@ -76,61 +95,133 @@ const EditPriceCalculationModal = ({ calculationId, filters }: Props) => {
     });
   }
 
-  return (
-    <Box mb={SPACE.LG} px={SPACE.SM}>
-      {(isLoading || isRefetching) && <SpinnerOverlay fillContainer={true} />}
-      <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(submitForm)}>
-          <ProductDevelopmentModalTopSection
-            productDevelopment={priceCalculation?.productDevelopmentDataDto}
-            sourcingCompanyCode={priceCalculation?.sourcingCompanyCode}
-            vendorName={priceCalculation?.vendorName}
-            actionBar={
-              <PriceCalculationActionBar
-                artwork={priceCalculation?.productDevelopmentDataDto?.artwork}
-                createNew={false}
-                lastModified={priceCalculation?.lastModified ?? ''}
-                id={priceCalculation?.id ?? ''}
-                showChanges={showChanges}
-                setShowChanges={(s: boolean) => setShowChanges(s)}
-              />
-            }
-          />
-          <Skeleton isLoaded={!!priceCalculation}>
-            <PriceCalculationForm
-              calculation={priceCalculation}
-              currencyCode={priceCalculation?.currencyCode ?? undefined}
-              createNew={false}
-              showChanges={showChanges}
-              productionId={priceCalculation?.productionId}
-            />
-          </Skeleton>
-        </form>
-      </FormProvider>
+  useEffect(() => {
+    setUnsavedChanges(form.formState.isDirty);
+    setPreventClose(form.formState.isDirty);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [form.formState.isDirty]);
 
-      <HStack justify={'space-between'} py={SPACE.XL}>
-        <Button
-          color={COLORS.BLACK}
-          variant={'link'}
-          leftIcon={<i className="ri-arrow-left-line" />}
-          isDisabled={!priceCalculationNavigation?.previous}
-          onClick={() =>
-            setActiveCalculationId(priceCalculationNavigation?.previous ?? '')
-          }>
-          {`${t('Common.Previous')} ${t('PD.FilterLabel.vendor')}`}
-        </Button>
-        <Button
-          color={COLORS.BLACK}
-          variant={'link'}
-          rightIcon={<i className="ri-arrow-right-line" />}
-          isDisabled={!priceCalculationNavigation?.next}
-          onClick={() =>
-            setActiveCalculationId(priceCalculationNavigation?.next ?? '')
-          }>
-          {`${t('Common.Next')} ${t('PD.FilterLabel.vendor')}`}
-        </Button>
-      </HStack>
-    </Box>
+  useEffect(() => {
+    setPendingCalculationId(undefined);
+  }, [activeCalculationId]);
+
+  const openLeavePageModal = () => {
+    modalRef.current?.onOpen();
+  };
+
+  const onNavigate = (calculationId: string) => {
+    setPendingCalculationId(calculationId);
+
+    if (hasUnsavedChanges()) {
+      openLeavePageModal();
+    } else {
+      setActiveCalculationId(calculationId);
+    }
+  };
+
+  useOutsideClick({
+    ref: outsideRef,
+    handler: () => {
+      if (hasUnsavedChanges()) {
+        openLeavePageModal();
+        setPreventClose(true);
+      } else {
+        close();
+      }
+    },
+  });
+
+  useEffect(() => {
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (hasUnsavedChanges() && e.key === 'Escape') {
+        openLeavePageModal();
+      }
+    };
+
+    window.addEventListener('keyup', handleKeyUp);
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  });
+
+  return (
+    <>
+      <LeavePageModal
+        ref={modalRef}
+        onConfirm={() => {
+          setPreventClose(false);
+          modalRef.current?.onClose();
+          discardChanges();
+          if (pendingCalculationId) {
+            setActiveCalculationId(pendingCalculationId);
+          } else {
+            close();
+          }
+        }}
+      />
+
+      <Box mb={SPACE.LG} px={SPACE.SM} ref={outsideRef}>
+        {(isLoading || isRefetching) && <SpinnerOverlay fillContainer={true} />}
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(submitForm)}>
+            <ProductDevelopmentModalTopSection
+              productDevelopment={priceCalculation?.productDevelopmentDataDto}
+              sourcingCompanyCode={priceCalculation?.sourcingCompanyCode}
+              vendorName={priceCalculation?.vendorName}
+              actionBar={
+                <PriceCalculationActionBar
+                  artwork={priceCalculation?.productDevelopmentDataDto?.artwork}
+                  createNew={false}
+                  lastModified={priceCalculation?.lastModified ?? ''}
+                  id={priceCalculation?.id ?? ''}
+                  showChanges={showChanges}
+                  setShowChanges={(s: boolean) => setShowChanges(s)}
+                />
+              }
+            />
+            <Skeleton isLoaded={!!priceCalculation}>
+              <PriceCalculationForm
+                calculation={priceCalculation}
+                currencyCode={priceCalculation?.currencyCode ?? undefined}
+                createNew={false}
+                showChanges={showChanges}
+                productionId={priceCalculation?.productionId}
+              />
+            </Skeleton>
+          </form>
+        </FormProvider>
+        <ContentSection>
+          <Grid justifyContent={'space-between'} display={'flex'} py={GRID.GAP}>
+            <GridItem>
+              {priceCalculationNavigation?.previous && (
+                <ArrowLink
+                  direction={'left'}
+                  onClick={() => {
+                    if (priceCalculationNavigation?.previous) {
+                      onNavigate(priceCalculationNavigation.previous);
+                    }
+                  }}>
+                  <>{t('Common.Previous')}</>
+                </ArrowLink>
+              )}
+            </GridItem>
+            <GridItem>
+              {priceCalculationNavigation?.next && (
+                <ArrowLink
+                  direction={'right'}
+                  onClick={() => {
+                    if (priceCalculationNavigation?.next) {
+                      onNavigate(priceCalculationNavigation.next);
+                    }
+                  }}>
+                  <>{t('Common.Next')}</>
+                </ArrowLink>
+              )}
+            </GridItem>
+          </Grid>
+        </ContentSection>
+      </Box>
+    </>
   );
 };
 
