032122c4d7a770e1d1bd27ba8d91789e48ae170f
Linnea Karlsson <linnea.d.karlsson@gmail.com>
2024-04-23 13:51:13 +0200

    6610 Preview file

--- src/app/utils/file.ts
+++ src/app/utils/file.ts
@@ -8,3 +8,25 @@ export const downloadBlob = (data: Blob, filename: string) => {
   link.remove();
   window.URL.revokeObjectURL(blobUrl);
 };
+
+// TODO: Draft of workaround
+export const previewBlob = (
+  data: Blob,
+  filename: string,
+  contentType: string
+) => {
+  const blobUrl = window.URL.createObjectURL(
+    new Blob([data], { type: contentType })
+  );
+  const newTab = window.open();
+  if (newTab) {
+    newTab.document.write(
+      `<iframe src="${blobUrl}" style="width: 100%; height: 100%; border: none;"></iframe>`
+    );
+    newTab.document.title = filename;
+  } else {
+    console.error(
+      'Failed to open preview in new tab. Please check your browser settings.'
+    );
+  }
+};
--- src/components/Button/ArtworkButton.tsx
+++ src/components/Button/ArtworkButton.tsx
@@ -19,6 +19,7 @@ const ArtworkButton = ({ size, artwork }: Props) => {
   );
 
   const handleClick = () => {
+    // TODO: Preview
     downloadFile();
   };
 
