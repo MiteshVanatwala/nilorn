7721e9ef9424e392a1d8ccbebc064e907551aec3
Linnea Karlsson <linnea.d.karlsson@gmail.com>
2024-04-24 15:12:20 +0200

    6671 Always refetch data when overview loads

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,7 +1,9 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { PaginationProvider } from '../../app/context/PaginationProvider';
 import { useForm } from 'react-hook-form';
 import FormuQuerySubmit from '../../components/Form/FormQuerySubmit';
+import QueryKeysEnum from '../../app/api/queryKeys';
+import { useQueryClient } from 'react-query';
 const OverviewTableContainer = lazy(() => import('./OverviewTableContainer'));
 const ProductDevelopmentFilter = lazy(
   () => import('../../components/Filter/ProductDevelopmentFilter')
@@ -9,6 +11,11 @@ const ProductDevelopmentFilter = lazy(
 
 function Overview() {
   const form = useForm();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.invalidateQueries([QueryKeysEnum.Overview]);
+  }, [queryClient]);
 
   return (
     <PaginationProvider>
