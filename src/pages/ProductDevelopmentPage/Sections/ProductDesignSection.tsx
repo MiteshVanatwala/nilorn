1f1c304223f4ea7b1ba4a1f94201f6d173fe9516
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-05-22 11:37:34 +0200

    Replaced number inputfields with IntegerInputField components in ProductDesignSection

--- src/pages/ProductDevelopmentPage/Sections/ProductDesignSection.tsx
+++ src/pages/ProductDevelopmentPage/Sections/ProductDesignSection.tsx
@@ -3,11 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { GRID } from '../../../theme/Constants';
 import AccordionItem from '../../../components/AccordionItem/AccordionItem';
 import Select from '../../../components/Form/Select';
-import InputField from '../../../components/Form/InputField';
 import { useWatch } from 'react-hook-form';
 import useFilterOptions from '../../../app/hooks/useFilterOption';
 import { SelectOption } from '../../../app/types/types';
 import SelectSkeleton from '../../../components/Form/SelectSkeleton';
+import IntegerInputField from '../../../components/Form/IntegerInputField';
 
 type Props = {
   disableEdit: boolean;
@@ -47,31 +47,28 @@ const ProductDesignSection = ({ disableEdit }: Props) => {
         <GridItem colSpan={12}>
           <Grid gap={GRID.GAP} templateColumns={GRID.TEMPLATE_COLUMNS}>
             <GridItem colSpan={2}>
-              <InputField
+              <IntegerInputField
                 label={`${t('PD.FormContent.FinishedLengthMM')}`}
                 placeholder={`${t('Common.Placeholder')}`}
                 name={'finishedLength'}
-                type="number"
                 registerOptions={{ valueAsNumber: true }}
                 readonly={disableEdit}
               />
             </GridItem>
             <GridItem colSpan={2}>
-              <InputField
+              <IntegerInputField
                 label={`${t('PD.FormContent.FinishedWidthMM')}`}
                 placeholder={`${t('Common.Placeholder')}`}
                 name={'finishedWidth'}
-                type="number"
                 registerOptions={{ valueAsNumber: true }}
                 readonly={disableEdit}
               />
             </GridItem>
             <GridItem colSpan={2}>
-              <InputField
+              <IntegerInputField
                 label={`${t('PD.FormContent.FinishedHeightMM')}`}
                 placeholder={`${t('Common.Placeholder')}`}
                 name={'finishedHeight'}
-                type="number"
                 registerOptions={{ valueAsNumber: true }}
                 readonly={disableEdit}
               />
