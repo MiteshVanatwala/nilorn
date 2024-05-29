02d0c053a06458e98dfa896b02f32f679f75aa79
Lucas Ahlgren <lucas.ahlgren@toxic.se>
2024-05-29 12:11:03 +0200

    RemixIcon updated

--- src/components/Icon/RemixIcon.tsx
+++ src/components/Icon/RemixIcon.tsx
@@ -2,26 +2,39 @@ import { Text, TextProps } from '@chakra-ui/react';
 import { HTMLAttributes } from 'react';
 import { REMIX_ICONS } from '../../theme/Constants';
 
-type RemixIconProps = {
-  component?: 'Text' | 'i';
+type RemixIconTextProps = {
+  component?: 'Text';
   icon: keyof typeof REMIX_ICONS.CLASS_NAMES;
-};
+} & TextProps;
 
-type RemixIconTextProps = RemixIconProps & TextProps;
-type RemixIconIProps = RemixIconProps & HTMLAttributes<HTMLElement>;
+type RemixIconIProps = {
+  component?: 'i';
+  icon: keyof typeof REMIX_ICONS.CLASS_NAMES;
+} & HTMLAttributes<HTMLElement>;
 
 type Props = RemixIconIProps | RemixIconTextProps;
 
-const RemixIcon = ({ icon, component = 'i', style, ...rest }: Props) => {
+const RemixIcon = ({
+  icon,
+  component = 'i',
+  style,
+  className,
+  ...rest
+}: Props) => {
   switch (component) {
     case 'i':
-      return <i style={style} className={REMIX_ICONS.CLASS_NAMES[icon]} />;
+      return (
+        <i
+          style={style}
+          className={className ?? REMIX_ICONS.CLASS_NAMES[icon]}
+        />
+      );
     case 'Text':
       return (
         <Text
           {...rest}
           style={style}
-          className={REMIX_ICONS.CLASS_NAMES[icon]}
+          className={className ?? REMIX_ICONS.CLASS_NAMES[icon]}
         />
       );
     default:
