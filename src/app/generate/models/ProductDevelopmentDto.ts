3c7ba7ebd04648df6725ab67814385522cc7c814
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-05-30 08:26:32 +0200

    Generated updated api

--- src/app/generate/models/ProductDevelopmentDto.ts
+++ src/app/generate/models/ProductDevelopmentDto.ts
@@ -5,6 +5,7 @@
 
 import type { MediaFileDto } from './MediaFileDto';
 import type { MemberBriefDto } from './MemberBriefDto';
+import type { ProductDevelopmentVersionDto } from './ProductDevelopmentVersionDto';
 import type { SourcingDto } from './SourcingDto';
 import type { Status } from './Status';
 
@@ -29,7 +30,7 @@ export type ProductDevelopmentDto = {
     artwork?: MediaFileDto;
     client?: string | null;
     clientNo?: string | null;
-    versions?: number | null;
+    versions?: Array<ProductDevelopmentVersionDto> | null;
     sourcings?: Array<SourcingDto> | null;
     members?: Array<MemberBriefDto> | null;
     hasProductions?: boolean;
