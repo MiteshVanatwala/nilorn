3f9e1be56a0dcd0bc3539b8ff4712c13132275eb
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-04 08:08:01 +0200

    Created PriceGridInput and replaced inputs in SalesPriceCalculation with it

--- src/pages/PriceCalculationsPage/PriceGrid/SalesPriceCalculation.tsx
+++ src/pages/PriceCalculationsPage/PriceGrid/SalesPriceCalculation.tsx
@@ -1,14 +1,13 @@
-import { ChangeEvent } from 'react';
+
 import { GridTd } from '../../../components/GridTable/GridTableElements';
 import { PriceCalculationDto, PriceDto } from '../../../app/generate';
-import { Input } from '@chakra-ui/react';
 import {
   calculateMargin,
   calculateSalesPrice,
 } from '../../../app/utils/price/PriceHelper';
-import { BORDER_RADIUS, SPACE } from '../../../theme/Constants';
-import { MAX_MARGIN, STEP } from '../../../app/utils/constant';
+import { MAX_MARGIN } from '../../../app/utils/constant';
 import { numToThousandSeparatedsStr, roundUp } from '../../../app/utils/common';
+import PriceGridInput from './PriceGridInput';
 
 type Props = {
   enableEdit: boolean;
@@ -33,33 +32,24 @@ const SalesPriceCalculation = ({
   salesPrice,
   onCalculationChange,
 }: Props) => {
-  const changeMargin = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = Number(e.target.value);
-    value = isNaN(value) ? 0 : value;
-    const max = Number(e.target.max) || MAX_MARGIN;
-    value = Math.min(value, max);
-
+  const changeMargin = (newMargin: number) => {
     const newSalesPrice = calculateSalesPrice(
       price.cost ?? 0,
       calculation.freightIncluded ?? 0,
-      value
+      newMargin
     );
-    onCalculationChange(value, newSalesPrice ?? 0, salesPriceId);
+
+    onCalculationChange(newMargin, newSalesPrice ?? 0, salesPriceId);
   };
 
-  const changeSalesPrice = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = Number(e.target.value);
-    value = isNaN(value) ? 0 : value;
-    const min = Number(e.target.min) || 0;
-    value = Math.max(value, min);
-
+  const changeSalesPrice = (newPrice: number) => {
     const newMargin = calculateMargin(
-      value,
+      newPrice,
       price.cost ?? 0,
       calculation.freightIncluded ?? 0
     );
 
-    onCalculationChange(newMargin ?? 0, value, salesPriceId);
+    onCalculationChange(newMargin ?? 0, newPrice, salesPriceId);
   };
 
   return (
@@ -72,15 +62,10 @@ const SalesPriceCalculation = ({
       <GridTd>
         <>
           {enableEdit ? (
-            <Input
+            <PriceGridInput
               onChange={changeMargin}
-              value={margin ?? 0}
+              value={margin}
               max={MAX_MARGIN}
-              type="number"
-              step={STEP}
-              variant={'outline'}
-              my={SPACE.XXS}
-              borderRadius={BORDER_RADIUS.XS}
             />
           ) : (
             <>
@@ -94,15 +79,10 @@ const SalesPriceCalculation = ({
       <GridTd>
         <>
           {enableEdit ? (
-            <Input
+            <PriceGridInput
               onChange={changeSalesPrice}
-              value={salesPrice ?? 0}
+              value={salesPrice}
               min={0}
-              type="number"
-              step={STEP}
-              variant={'outline'}
-              my={SPACE.XXS}
-              borderRadius={BORDER_RADIUS.XS}
             />
           ) : (
             <>
