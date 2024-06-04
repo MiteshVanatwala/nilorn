f0253ce33ea517bbb74f30fcc47290775c40af87
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-04 09:10:44 +0200

    Removed select onFocus for TeaxtArea

--- src/components/Form/TextArea.tsx
+++ src/components/Form/TextArea.tsx
@@ -49,7 +49,6 @@ const TextArea = ({
         placeholder={placeholder}
         height={'auto'}
         readOnly={readonly}
-        onFocus={e => e.target.select()}
         {...register(name, registerOptions)}
       />
     </ControlWrapper>
