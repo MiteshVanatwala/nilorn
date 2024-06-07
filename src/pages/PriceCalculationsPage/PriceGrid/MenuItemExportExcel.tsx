6ae2550eb4d97fa6b36cdf0f485119be192b325c
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-07 09:46:56 +0200

    Added export excel option for price calculation table

---
+++ src/pages/PriceCalculationsPage/PriceGrid/MenuItemExportExcel.tsx
@@ -0,0 +1,34 @@
+import { MenuItem } from '@chakra-ui/react';
+import RemixIcon from '../../../components/Icon/RemixIcon';
+import { SIZES } from '../../../theme/Constants';
+import { useTranslation } from 'react-i18next';
+import { useDownloadFile } from '../../../app/hooks/useDownloadFile';
+
+type Props = {
+  priceCalculationId: string;
+};
+
+const MenuItemExportExcel = ({ priceCalculationId }: Props) => {
+  const { t } = useTranslation();
+  const { downloadFile } = useDownloadFile();
+
+  const onExportExcel = () => {
+    downloadFile(
+      `/api/Excel/GetExcel/${priceCalculationId}`,
+      `Export-Calculation-${priceCalculationId}.xlsx`,
+      'GET'
+    );
+  };
+
+  return (
+    <MenuItem
+      onClick={onExportExcel}
+      icon={
+        <RemixIcon component="Text" fontSize={SIZES.ICON.MD} icon={'EXCEL'} />
+      }>
+      {t('PriceCalc.ExportToExcel')}
+    </MenuItem>
+  );
+};
+
+export default MenuItemExportExcel;
