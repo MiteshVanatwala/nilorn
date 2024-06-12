b66b78cb70d0559dbea8c6b55fdd2ffd08b8a8d8
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-12 15:59:16 +0200

    Removed create new button from filter section on Productions and PriceCalculations

--- src/pages/Productions/ProductionsFilter.tsx
+++ src/pages/Productions/ProductionsFilter.tsx
@@ -4,7 +4,7 @@ import FilterSection from '../../components/Filter/FilterSection';
 const STANDARD_KEYS: FilterKeys[] = ['clients', 'projects', 'vendors'];
 
 const ProductionsFilter = () => {
-  return <FilterSection createNew standardFilterKeys={STANDARD_KEYS} />;
+  return <FilterSection standardFilterKeys={STANDARD_KEYS} />;
 };
 
 export default ProductionsFilter;
