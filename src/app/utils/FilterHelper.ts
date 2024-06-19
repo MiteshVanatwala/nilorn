c2d0266411708815b1f781812ea1e528011b9d0f
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-19 08:52:50 +0200

    Set back value for pageNumber and pageSize, and emptied searchQuery in clearFilters

--- src/app/utils/FilterHelper.ts
+++ src/app/utils/FilterHelper.ts
@@ -194,10 +194,16 @@ export function convertQueryStringToFilterObject(
   return result;
 }
 export function useClearAllFilters() {
-  const { unregister } = useFormContext();
+  const { unregister, getValues, setValue } = useFormContext();
 
   const clearFilters = () => {
+    const pageNumber = getValues('pageNumber');
+    const pageSize = getValues('pageSize');
+    const searchQuery = '';
     unregister(allFilterKeys);
+    setValue('pageNumber', pageNumber);
+    setValue('pageSize', pageSize);
+    setValue('searchQuery', searchQuery);
     const storedFilter = getCurrentStoredFilter();
     sessionStorage.setItem(storedFilter, '');
   };
