f25c58571e66a28cf8a5f44962a2843e2c2a4fc5
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-19 14:50:45 +0200

    Changed InputSearch to be a controlled input with debounce behaviour

--- src/components/Form/InputSearch.tsx
+++ src/components/Form/InputSearch.tsx
@@ -1,5 +1,8 @@
+import { useFormContext, useWatch } from 'react-hook-form';
 import { FormInputProps } from '../../app/types/types';
-import InputField from './InputField';
+import { ChangeEvent, useEffect, useState } from 'react';
+import ControlWrapper from './ControlWrapper';
+import { Input } from '@chakra-ui/react';
 
 interface Props extends FormInputProps {
   placeholder?: string;
@@ -7,19 +10,45 @@ interface Props extends FormInputProps {
 }
 
 const InputSearch = ({ name, label, placeholder, variant }: Props) => {
+  const { setValue, unregister } = useFormContext();
+  const [searchValue, setSearchValue] = useState('');
+  const watch = useWatch({ name: name });
+
+  useEffect(() => {
+    if (!!watch) {
+      setSearchValue(watch);
+    } else {
+      setSearchValue('');
+      unregister(name);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [watch]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setValue(name, encodeURIComponent(searchValue));
+    }, 400);
+    return () => {
+      clearTimeout(handler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchValue]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
-    <InputField
-      label={label}
-      variant={variant}
-      type={'search'}
-      placeholder={placeholder}
-      name={name}
-      registerOptions={{
-        setValueAs(value) {
-          return encodeURIComponent(value);
-        },
-      }}
-    />
+    <>
+      <ControlWrapper name={name} label={label}>
+        <Input
+          value={searchValue}
+          variant={variant}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      </ControlWrapper>
+    </>
   );
 };
 
