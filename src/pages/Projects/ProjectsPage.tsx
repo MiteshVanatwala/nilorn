e2675d5c0a0d936a030526b943c9e50b26fd1125
Henric Hiljanen <henric.hiljanen@toxic.se>
2024-06-26 22:50:58 +0200

    Added ProjectGeneralSection and MembersSection to ProjetcsPage

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,6 +1,10 @@
 import ContentPage from '../Templates/ContentPage';
+import { Accordion } from '@chakra-ui/react';
 import { SelectOption } from '../../app/types/types';
 import { useEffect, useMemo, useState } from 'react';
+import ProjectGeneralSection from './Sections/ProjectGeneralSection';
+import { SPACE } from '../../theme/Constants';
+import MemberSection from '../ProductDevelopmentPage/Sections/MemberSection';
 import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import ProjectsTopSection from './Sections/ProjectsTopSection/ProjectsTopSection';
 import { useGetProjectCard } from '../../app/api/Projects';
@@ -49,6 +53,15 @@ function ProjectsPage() {
             lastModified={new Date()}
             clientOptions={clientOptions}
           />
+
+          <Accordion
+            variant={'card'}
+            defaultIndex={[0, 1, 2, 3, 4]}
+            marginBottom={SPACE.XXL}
+            allowMultiple>
+            <ProjectGeneralSection />
+            <MemberSection disableEdit={false} />
+          </Accordion>
         </form>
       </FormProvider>
     </ContentPage>
