df2cd0d2655487de5e67ef07be00fa883a04c633
Mitesh Vanatwala <mitesh@gobrillianttech.com>
2024-12-11 15:49:42 +0530

    #7407 - Sort Quantity in ascending order

--- src/pages/ProductDevelopmentPage/Sections/SourcingSection.tsx
+++ src/pages/ProductDevelopmentPage/Sections/SourcingSection.tsx
@@ -23,7 +23,7 @@ const SourcingSection = ({ no, disableEdit }: Props) => {
 
   const { t } = useTranslation();
 
-  const { getValues, control } = useFormContext();
+  const { getValues, control, setValue } = useFormContext();
 
   const {
     fields: sourcings,
@@ -36,6 +36,17 @@ const SourcingSection = ({ no, disableEdit }: Props) => {
 
   const sourcingCompanies = useFilterOptions('sourcingCompanies');
   const [selected, setSelected] = useState<MultiValue<SelectOption>>([]);
+
+  useEffect(() => {
+    for (let index = 0; index < sourcings.length; index++) {
+      setValue(
+        `${SOURCING_KEY}.${index}.quantities`,
+        getValues(`${SOURCING_KEY}.${index}.quantities`).sort(
+          (a: number, b: number) => a - b
+        )
+      );
+    }
+  }, [sourcings]);
 
   useEffect(() => {
     if (sourcingCompanies.length > 0) {
