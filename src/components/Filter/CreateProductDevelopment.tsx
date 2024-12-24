5c8d4882126df42368ebebfe1b51ad84b0228453
Mitesh Vanatwala <mitesh@gobrillianttech.com>
2024-12-24 17:47:54 +0530

    #7458 - 2 "buttons" for Create new

--- src/components/Filter/CreateProductDevelopment.tsx
+++ src/components/Filter/CreateProductDevelopment.tsx
@@ -12,14 +12,14 @@ const CreateProductDevelopment = () => {
   if (user?.role && ROLES_ALLOWED_TO_CREATE.includes(user.role)) {
     return (
       <Box textAlign={'right'}>
-        <Link as={NavLink} to={'/product-development/create'}>
-          <Button
-            alignSelf={'end'}
-            rightIcon={<RemixIcon component="i" icon="ADD_LINE" />}
-            variant={'primary'}>
-            {t('Common.CreateNew')}
-          </Button>
-        </Link>
+        <Button
+          as={NavLink}
+          to={'/product-development/create'}
+          alignSelf={'end'}
+          rightIcon={<RemixIcon component="i" icon="ADD_LINE" />}
+          variant={'primary'}>
+          {t('Common.CreateNew')}
+        </Button>
       </Box>
     );
   }
