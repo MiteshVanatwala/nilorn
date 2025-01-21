4a60f4b04fdbbfd01404db0e04b75633a2aa5783
Mitesh Vanatwala <mitesh@gobrillianttech.com>
2025-01-21 17:35:42 +0530

    #7391 - Same gray shade of disabled fields

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -15,4 +15,4 @@ export const INCLUDE_CLOSED = 'includeClosed';
 export const SEARCH_QUERY = 'searchQuery';
 export const PAGE_SIZE = 'pageSize';
 
-export const READ_ONLY_OPACITY = '40%';
+export const READ_ONLY_OPACITY = '70%';
