b826b9eace9d2946a35905e8a564c8fdd654f207
Marcus Larsson <marcus.larsson@nilorn.com>
2025-02-13 15:45:38 +0100

    In progress

--- src/pages/PriceCalculationsPage/PriceCalculationsTable.tsx
+++ src/pages/PriceCalculationsPage/PriceCalculationsTable.tsx
@@ -1,13 +1,16 @@
+// PriceCalculationsTable.tsx
 import { useTranslation } from 'react-i18next';
+import { Fragment, useState } from 'react';
 import { ProductDevelopmentDeepDto } from '../../app/generate';
 import {
   GridTh,
   GridTable,
 } from '../../components/GridTable/GridTableElements';
 import PriceCalculationsTableRow from './PriceCalculationsTableRow';
+import { Button } from '@chakra-ui/react';
 
 const GRID_LAYOUT =
-  'repeat(4, minmax(100px, 1fr)) [Vendor] minmax(100px, 1fr) [Comment] 1fr [BaseValues] minmax(100px, 1fr) repeat(8, minmax(100px, 1fr))';
+  'minmax(50px, 0.5fr) repeat(4, minmax(100px, 1fr)) [Vendor] minmax(100px, 1fr) [Comment] 1fr [BaseValues] minmax(100px, 1fr) repeat(8, minmax(100px, 1fr))';
 
 export const GRID_LAYOUT_SOURCING =
   'repeat(1, minmax(100px, 1fr)) [Vendor] minmax(100px, 1fr) [Comment] 1fr [BaseValues] minmax(100px, 1fr) repeat(8, minmax(100px, 1fr))';
@@ -16,7 +19,7 @@ export const GRID_LAYOUT_PRICE =
   '[Vendor] minmax(100px, 1fr) [Comment] 1fr [BaseValues] minmax(100px, 1fr) repeat(8, minmax(100px, 1fr))';
 
 const GRID_LAYOUT_DESKTOP =
-  'repeat(4, 1fr) [Vendor] minmax(150px, 1fr) [Comment] 1fr [BaseValues] minmax(150px, 1fr) repeat(8, 1fr)';
+  'minmax(50px, 0.5fr) repeat(4, 1fr) [Vendor] minmax(150px, 1fr) [Comment] 1fr [BaseValues] minmax(150px, 1fr) repeat(8, 1fr)';
 
 export const GRID_LAYOUT_SOURCING_DESKTOP =
   'repeat(1, 1fr) [Vendor] minmax(150px, 1fr) [Comment] 1fr [BaseValues] minmax(150px, 1fr) repeat(8, 1fr)';
@@ -32,16 +35,82 @@ export const VENDOR_ROW_SPAN = 11;
 export const CALCULATION_ROW_SPAN = 9;
 export const PRICE_ROW_SPAN = 4;
 
+export type SelectedPrices = {
+  [key: string]: boolean;
+};
+
 type Props = {
   data: ProductDevelopmentDeepDto[];
 };
 
 const PriceCalculationsTable = ({ data }: Props) => {
   const { t } = useTranslation();
+  const [selectedPrices, setSelectedPrices] = useState<SelectedPrices>({});
+
+  const handleExportClick = async () => {
+    const selectedIds = Object.entries(selectedPrices)
+      .filter(([_, isSelected]) => isSelected)
+      .map(([id]) => id);
+
+    const excelExportOptions = selectedIds.map(id => ({
+      priceCalculationId: id,
+      valid: true,
+      included: true,
+      no: true,
+      name: true,
+      itemNo: true,
+      description: true,
+      version: true,
+      quantity: true,
+      certificate: true,
+      salesPrice: true,
+      salesCurrency: true,
+      purchaseCurrency: true,
+      purchasePrice: true,
+      moq: true
+    }));
+
+    try {
+      const response = await fetch('/api/pricecalculations/GetExcel', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(excelExportOptions)
+      });
+
+      if (response.ok) {
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'price-calculations.xlsx';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      } else {
+        console.error('Export failed');
+      }
+    } catch (error) {
+      console.error('Export error:', error);
+    }
+  };
+
   return (
     <>
+      <Button
+        colorScheme="blue"
+        onClick={handleExportClick}
+        mb={4}
+        isDisabled={!Object.values(selectedPrices).some(Boolean)}
+      >
+        {t('Export Selected')}
+      </Button>
+      
       <GridTable
         gridTemplateColumns={{ base: GRID_LAYOUT, lg: GRID_LAYOUT_DESKTOP }}>
+          
         <GridTh colSpan={PD_COL_SPAN}>
           {t('Production.ProductDevelopments')}
         </GridTh>
@@ -58,11 +127,17 @@ const PriceCalculationsTable = ({ data }: Props) => {
         <GridTh>{t('PriceCalc.Margin')}</GridTh>
         <GridTh>{t('PriceCalc.Sales')}</GridTh>
         <GridTh />
-        <>
-          {data.map((p, i) => (
-            <PriceCalculationsTableRow key={i} productDevelopment={p} />
-          ))}
-        </>
+        
+        <Fragment>
+        {data.map((p, i) => (
+          <PriceCalculationsTableRow 
+            key={i} 
+            productDevelopment={p}
+            selectedPrices={selectedPrices}
+            setSelectedPrices={setSelectedPrices}
+          />
+        ))}
+        </Fragment>
       </GridTable>
     </>
   );
--- src/pages/PriceCalculationsPage/PriceCalculationsTableRow.tsx
+++ src/pages/PriceCalculationsPage/PriceCalculationsTableRow.tsx
@@ -1,11 +1,11 @@
-import { Fragment } from 'react';
+import { Fragment, ReactElement } from 'react';
 import { ProductDevelopmentDeepDto } from '../../app/generate';
 import {
   GridInlineTbody,
   GridTd,
 } from '../../components/GridTable/GridTableElements';
 import ProductDevelopmentCell from '../../components/ProductDevelopment/ProductDevelopmentCell';
-import { GridItem } from '@chakra-ui/react';
+import { GridItem, Checkbox } from '@chakra-ui/react';
 import { TD_STYLE } from '../../theme/Constants/tableGrid';
 import PriceGridRow from './PriceGrid/PriceGridRow';
 import {
@@ -15,18 +15,64 @@ import {
   GRID_LAYOUT_SOURCING_DESKTOP,
   PD_COL_SPAN,
   SOURCING_COL_SPAN,
-  VENDOR_ROW_SPAN,
+  VENDOR_ROW_SPAN
 } from './PriceCalculationsTable';
+import { SelectedPrices } from './PriceCalculationsTable';
 
 type Props = {
   productDevelopment: ProductDevelopmentDeepDto;
+  selectedPrices: SelectedPrices;
+  setSelectedPrices: React.Dispatch<React.SetStateAction<SelectedPrices>>;
 };
 
-const PriceCalculationsTableRow = ({ productDevelopment: p }: Props) => {
+const PriceCalculationsTableRow = ({ 
+  productDevelopment: p,
+  selectedPrices,
+  setSelectedPrices
+}: Props) => {
+  const handleCheckboxChange = (productionId: string) => {
+    setSelectedPrices((prev: SelectedPrices) => {
+      const isChecked = !prev[productionId];
+
+      // Log only when the checkbox is checked
+      if (isChecked) {
+        console.log(`Checkbox checked for productionId: ${productionId}`);
+      }
+
+      return {
+        ...prev,
+        [productionId]: isChecked
+      };
+    });
+  };
+
+  const renderCheckboxes = (): ReactElement[] => {
+    const checkboxes: ReactElement[] = [];
+    
+    p.sourcedProductions?.forEach(s => {
+      s.productions?.forEach(production => {
+        if (production.id) {
+          checkboxes.push(
+            <Checkbox
+              key={production.id}
+              isChecked={selectedPrices[production.id] || false}
+              onChange={() => handleCheckboxChange(production.id || "")}
+            />
+          );
+        }
+      });
+    });
+
+    return checkboxes.length > 0 ? checkboxes : [<span key="no-checkboxes">No checkboxes available</span>];
+  };
+
   return (
     <Fragment key={p?.productDevelopmentDataDto?.no}>
-      <GridTd colSpan={PD_COL_SPAN} id={'1'}>
+      <GridTd colSpan={PD_COL_SPAN}>
         <ProductDevelopmentCell {...p.productDevelopmentDataDto} />
+      </GridTd>
+      <GridTd colSpan={1}>
+        <Fragment>{renderCheckboxes()}</Fragment>
       </GridTd>
       <GridTd>{p.productDevelopmentDataDto?.clientName ?? ''}</GridTd>
       <GridItem colSpan={VENDOR_ROW_SPAN + SOURCING_COL_SPAN}>
@@ -35,47 +81,39 @@ const PriceCalculationsTableRow = ({ productDevelopment: p }: Props) => {
             base: GRID_LAYOUT_SOURCING,
             lg: GRID_LAYOUT_SOURCING_DESKTOP,
           }}>
-          <>
-            {p.sourcedProductions?.map(s => (
-              <Fragment
-                key={p?.productDevelopmentDataDto?.no + '-' + s?.sourcingId}>
-                <GridTd
-                  colSpan={SOURCING_COL_SPAN}
-                  key={p?.productDevelopmentDataDto?.no + '-' + s?.sourcingId}
-                  style={TD_STYLE}>
-                  <>{s.sourcingCompanyCode}</>
-                </GridTd>
-                <GridItem colSpan={VENDOR_ROW_SPAN}>
-                  <GridInlineTbody
-                    gridTemplateColumns={{
-                      base: GRID_LAYOUT_PRICE,
-                      lg: GRID_LAYOUT_PRICE_DESKTOP,
-                    }}>
-                    <>
-                      {s.productions && s.productions?.length > 0 ? (
-                        s.productions?.map(production => (
-                          <PriceGridRow
-                            key={
-                              p?.productDevelopmentDataDto?.no +
-                              '-' +
-                              s?.sourcingId +
-                              '-' +
-                              production?.id
-                            }
-                            production={production}
-                            sourcedProduction={s}
-                            productDevelopment={p.productDevelopmentDataDto}
-                          />
-                        ))
-                      ) : (
-                        <GridTd colSpan={VENDOR_ROW_SPAN}></GridTd>
-                      )}
-                    </>
-                  </GridInlineTbody>
-                </GridItem>
-              </Fragment>
-            ))}
-          </>
+          {p.sourcedProductions?.map(s => (
+            <Fragment
+              key={`${p?.productDevelopmentDataDto?.no}-${s?.sourcingId}`}>
+              <GridTd
+                colSpan={SOURCING_COL_SPAN}
+                key={`${p?.productDevelopmentDataDto?.no}-${s?.sourcingId}`}
+                style={TD_STYLE}>
+                {s.sourcingCompanyCode}
+              </GridTd>
+              <GridItem colSpan={VENDOR_ROW_SPAN}>
+                <GridInlineTbody
+                  gridTemplateColumns={{
+                    base: GRID_LAYOUT_PRICE,
+                    lg: GRID_LAYOUT_PRICE_DESKTOP,
+                  }}>
+                  {s.productions && s.productions.length > 0 ? (
+                    s.productions.map(production => (
+                      <Fragment
+                        key={`${p?.productDevelopmentDataDto?.no}-${s?.sourcingId}-${production?.id}`}>
+                        <PriceGridRow
+                          production={production}
+                          sourcedProduction={s}
+                          productDevelopment={p.productDevelopmentDataDto}
+                        />
+                      </Fragment>
+                    ))
+                  ) : (
+                    <GridTd colSpan={VENDOR_ROW_SPAN} />
+                  )}
+                </GridInlineTbody>
+              </GridItem>
+            </Fragment>
+          ))}
         </GridInlineTbody>
       </GridItem>
     </Fragment>
