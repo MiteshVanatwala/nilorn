ea0914cfe2eac1d8e93ed25f729e4f4736bcd5e3
Mitesh Vanatwala <vmitesh.282@gmail.com>
2025-04-09 14:44:57 +0530

    #7757 - Excel export modal with spinner

--- src/components/ExcelExport/ExcelExportModalContent.tsx
+++ src/components/ExcelExport/ExcelExportModalContent.tsx
@@ -85,7 +85,7 @@ const ExcelExportModalContent = ({
     try {
       downloadFile(
         `${process.env.REACT_APP_API_URL}/api/Excel/GetExcel`,
-        'price-calculations.xlsx',
+        `${fileName.replace('.xls', '').replace('.xlsx', '').replace('.XLS', '').replace('.XLSX', '')}.xlsx`,
         'PUT',
         excelExportOptions
       ).then(() => {
