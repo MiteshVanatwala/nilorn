990b38f7f3d65c6005054f2b678502bcb202c58a
Mitesh Vanatwala <mitesh@gobrillianttech.com>
2025-07-11 15:24:47 +0530

    #7888 - Lead time issues

--- src/components/Form/RangeNumberInputField.tsx
+++ src/components/Form/RangeNumberInputField.tsx
@@ -38,9 +38,13 @@ const RangeNumberInputField = ({
   const [inputValue, setInputValue] = useState('');
   const minVal = useWatch({ name: `${name}Min` });
   const maxVal = useWatch({ name: `${name}Max` });
+  const editted = useWatch({ name: `${name}_editted` });
 
   useEffect(() => {
-    if (
+    if (editted) {
+      setInputValue(editted);
+      return;
+    } else if (
       minVal !== undefined &&
       maxVal !== undefined &&
       minVal !== null &&
@@ -157,6 +161,9 @@ const RangeNumberInputField = ({
           autoComplete="off"
           onChange={e => {
             setInputValue(e.target.value);
+            setValue(`${name}_editted`, e.target.value, {
+              shouldValidate: true,
+            });
             onChange(e); // Use the extracted onChange
           }}
           onBlur={e => {
