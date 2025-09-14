5244fe4684f79ca7c52b65b62edf271ebd375255
Mitesh Vanatwala <mitesh@gobrillianttech.com>
2025-09-14 09:06:27 +0530

    #7977 - Enter doesn't activate checkboxes in modal

--- src/components/ExcelExport/ExcelExportModalContent.tsx
+++ src/components/ExcelExport/ExcelExportModalContent.tsx
@@ -124,8 +124,11 @@ const ExcelExportModalContent = ({ selectedPrices }: Props) => {
                     handleCheckboxChange(key as ExcelExportFieldKey)
                   }
                   onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-                    if (e.key === 'Enter')
+                    if (e.key === 'Enter') {
+                      e.preventDefault();
+                      e.stopPropagation();
                       handleCheckboxChange(key as ExcelExportFieldKey);
+                    }
                   }}
                 />
               </GridItem>
