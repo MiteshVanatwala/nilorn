53da58e5b12b12a6b5ad7ed82b873d1fc4e7392f
MarcusLa <marcus.larssson@nilorn.com>
2025-10-30 15:06:40 +0100

    Added props.

--- src/components/ExcelExport/ExcelExportModalContent.tsx
+++ src/components/ExcelExport/ExcelExportModalContent.tsx
@@ -91,7 +91,7 @@ const ExcelExportModalContent = ({ selectedPrices }: Props) => {
 
     try {
       downloadFile(
-        `${process.env.REACT_APP_API_URL}/api/Excel/GetExcel`,
+         `${process.env.REACT_APP_API_URL}/api/Excel/GetExcel`,
         `${fileName.replace(/xlsx/gi, '').replace(/xls/gi, '')}.xlsx`,
         'PUT',
         excelExportOptions
