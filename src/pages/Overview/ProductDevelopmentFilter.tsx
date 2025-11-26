ad94404e1579ec8ba7e0a22316a5f808b2c60e67
Robert Frödin <robertfrodin@users.noreply.github.com>
2025-11-26 19:10:46 +0100

    Updated ProductDevelopmentFilter to use user scoped filter options

--- src/pages/Overview/ProductDevelopmentFilter.tsx
+++ src/pages/Overview/ProductDevelopmentFilter.tsx
@@ -2,9 +2,10 @@ import { FilterKey } from '../../app/types/types';
 import FilterSection from '../../components/Filter/FilterSection';
 
 const STANDARD_KEYS: FilterKey[] = ['clients', 'projects', 'statuses'];
+const FILTER_VALUES_BY_USER = true;
 
 const ProductDevelopmentFilter = () => {
-  return <FilterSection createNew standardFilterKeys={STANDARD_KEYS} />;
+  return <FilterSection createNew standardFilterKeys={STANDARD_KEYS} filterByUser={FILTER_VALUES_BY_USER} />;
 };
 
 export default ProductDevelopmentFilter;
