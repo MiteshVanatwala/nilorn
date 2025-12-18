8af468a6b32618c0646c698b03995dea204fafa6
Robert Frödin <robertfrodin@users.noreply.github.com>
2025-12-18 15:58:02 +0100

    6896 Excel export (price calculation) - merge fixes to main

--- src/components/Button/DownloadButton.tsx
+++ src/components/Button/DownloadButton.tsx
@@ -2,23 +2,20 @@ import { IconButton, Tooltip } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { useDownloadFile } from '../../app/hooks/useDownloadFile';
 import RemixIcon from '../Icon/RemixIcon';
-import { DownloadFileType, Method } from '../../app/types/types';
 
 type Props = {
-  filetype: DownloadFileType;
-  fileId: string;
+  url: string;
   fileName: string;
   disabled?: boolean;
   tooltipText?: string;
-  method?: Method;
+  method?: 'GET' | 'POST';
 };
 
 const DownloadButton = ({
-  filetype,
-  fileId,
+  url,
   fileName,
   tooltipText,
-  method = Method.GET,
+  method = 'GET',
 }: Props) => {
   const { t } = useTranslation();
   const { isLoading, downloadFile } = useDownloadFile();
@@ -27,7 +24,7 @@ const DownloadButton = ({
     <IconButton
       variant={'ghost'}
       aria-label={t('Common.Download')}
-      onClick={() => downloadFile(filetype, fileId, fileName, method)}
+      onClick={() => downloadFile(url, fileName, method)}
       isLoading={isLoading}
       icon={<RemixIcon component="i" icon="DOWNLOAD_LINE" />}
     />
