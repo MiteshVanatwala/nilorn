85a26c67ca5626e57b7eba4681aabdd52ca416c6
Mitesh Vanatwala <vmitesh.282@gmail.com>
2025-12-18 12:00:51 +0530

    Distribution Company

--- src/app/hooks/useFilterOption.tsx
+++ src/app/hooks/useFilterOption.tsx
@@ -8,6 +8,7 @@ import {
   useVendors,
   useClientFilterOptions,
 } from '../api/FilterInfo';
+import { useGetDistributionCompaniesOption } from '../api/distributionCompanies';
 import { useGetProjectsOptions } from '../api/Projects';
 import { useCertificateCodes } from '../api/production';
 import { ClientDto, MemberBriefDto, VendorDto } from '../generate';
@@ -59,6 +60,9 @@ const useFilterOptions = (name?: FilterKey, filterByAccess?: boolean, isProductD
     isProductDevelopment ?? false
   );
   const { data: opComp } = useOpCompOption(name === 'opComps' ?? false);
+  const { data: distributionCompanies } = useGetDistributionCompaniesOption(
+    name === 'distributionCompanies'
+  );
   const { data: members } = useMembers(name === 'members' ?? false);
   const { data: foldingTypes } = useFoldingType(
     name === 'foldingTypes' ?? false
@@ -85,7 +89,8 @@ const useFilterOptions = (name?: FilterKey, filterByAccess?: boolean, isProductD
     clients: clients as SelectOption[],
     statuses: statuses,
     sourcingCompanies: sourcingCompanies as SelectOption[],
-    opComps: opComp as SelectOption[],
+    opComps: (distributionCompanies ?? opComp) as SelectOption[],
+    distributionCompanies: distributionCompanies as SelectOption[],
     foldingTypes: foldingTypes as SelectOption[],
     itemCategories: itemCategories as SelectOption[],
     productGroups: productGroups as SelectOption[],
