edb2ae6e116987fac9aebe7f46e892c24d8f8911
Mitesh Vanatwala <vmitesh.282@gmail.com>
2026-01-28 18:28:02 +0530

    Add distribution company handling in price calculation modal

--- src/app/generate/models/DefaultPriceCalculationDto.ts
+++ src/app/generate/models/DefaultPriceCalculationDto.ts
@@ -12,4 +12,5 @@ export type DefaultPriceCalculationDto = {
     internalCommission?: number | null;
     indirectCost?: number | null;
     currencyRate?: number | null;
+    distributionCompanyCode?: string | null;
 };
